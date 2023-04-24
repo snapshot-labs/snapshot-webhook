86faf09175ca7bec972faec1ae97cf38903d958a
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-04-24 20:34:13 +0400

    chore: use global snapshot code formatting standard

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -5,7 +5,5 @@ export function shortenAddress(str = '') {
 }
 
 export function sha256(str) {
-  return createHash('sha256')
-    .update(str)
-    .digest('hex');
+  return createHash('sha256').update(str).digest('hex');
 }
