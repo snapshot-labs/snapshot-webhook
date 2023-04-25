0d60d9bd75800573617f76bde1c739846bdacaf9
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-04-26 00:48:46 +0400

    feat: add retry strategy to channel's queue

--- src/queues/index.ts
+++ src/queues/index.ts
@@ -9,7 +9,16 @@ const connection = new Redis(process.env.REDIS_URL as string);
 
 export const messagesQueue = new Queue('messages', { connection });
 export const eventsQueue = new Queue('events', { connection });
-export const httpChannelQueue = new Queue('channel-http', { connection });
+export const httpChannelQueue = new Queue('channel-http', {
+  connection,
+  defaultJobOptions: {
+    attempts: 5,
+    backoff: {
+      type: 'exponential',
+      delay: 10000
+    }
+  }
+});
 
 let messagesWorker: Worker;
 let eventsWorker: Worker;
