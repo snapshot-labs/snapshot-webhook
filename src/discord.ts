e7b59b80e4f959a7db69ed645f212e152c63b50c
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-04-26 22:53:11 +0400

    feat: retry on discord errors

--- src/discord.ts
+++ src/discord.ts
@@ -89,17 +89,17 @@ const commands = [
 ];
 
 export async function start() {
-  const rest = new REST({ version: '10' }).setToken(token);
-
   try {
+    const rest = new REST({ version: '10' }).setToken(token);
+
     console.log('Started refreshing application (/) commands.');
     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
     console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
 
-  client.login(token);
+    client.login(token);
+  } catch (e) {
+    console.log('Unable to start discord bot', e);
+  }
 }
 export const setActivity = (message, url?) => {
   try {
--- src/queues/processors/notifications/discord.ts
+++ src/queues/processors/notifications/discord.ts
@@ -5,5 +5,14 @@ import type { Event, Proposal } from '../../../types';
 export default async (job: Job) => {
   const { event, proposalId }: { event: Event; proposalId: Proposal['id'] } = job.data;
 
-  sendEventToDiscordSubscribers(event.event, proposalId);
+  try {
+    sendEventToDiscordSubscribers(event.event, proposalId);
+  } catch (error: any) {
+    console.log(
+      '[worker:discord] Error sending notification, will retry or fail',
+      error.toString(),
+      JSON.stringify(error)
+    );
+    throw error;
+  }
 };
