77455686679d712646a09bbdf8625fcad582bed4
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-08-04 12:09:16 +0900

    chore: update lib

--- src/helpers/metrics.ts
+++ src/helpers/metrics.ts
@@ -3,40 +3,40 @@ import db from './mysql';
 import type { Express } from 'express';
 
 export default function initMetrics(app: Express) {
-  init(app);
+  init(app, {
+    whitelistedPath: [/^\/$/, /^\/api\/test$/]
+  });
 }
 
+new client.Gauge({
+  name: 'events_per_type_count',
+  help: 'Number of events per type',
+  labelNames: ['type'],
+  async collect() {
+    const result = await db.queryAsync(
+      `SELECT count(*) as count, event FROM events GROUP BY event`
+    );
+    result.forEach(async function callback(this: any, data) {
+      this.set({ type: data.event }, data.count);
+    }, this);
+  }
+});
 
-  new client.Gauge({
-    name: 'events_per_type_count',
-    help: 'Number of events per type',
-    labelNames: ['type'],
-    async collect() {
-      const result = await db.queryAsync(
-        `SELECT count(*) as count, event FROM events GROUP BY event`
-      );
-      result.forEach(async function callback(this: any, data) {
-        this.set({ type: data.event }, data.count);
-      }, this);
-    }
-  });
-
-  new client.Gauge({
-    name: 'subscribers_per_type_count',
-    help: 'Number of subscribers per type',
-    labelNames: ['type'],
-    async collect() {
-      this.set(
-        { type: 'http' },
-        (await db.queryAsync(`SELECT count(*) as count FROM subscribers`))[0].count as any
-      );
-      this.set(
-        { type: 'discord' },
-        (await db.queryAsync(`SELECT count(*) as count FROM subscriptions`))[0].count as any
-      );
-    }
-  });
-
+new client.Gauge({
+  name: 'subscribers_per_type_count',
+  help: 'Number of subscribers per type',
+  labelNames: ['type'],
+  async collect() {
+    this.set(
+      { type: 'http' },
+      (await db.queryAsync(`SELECT count(*) as count FROM subscribers`))[0].count as any
+    );
+    this.set(
+      { type: 'discord' },
+      (await db.queryAsync(`SELECT count(*) as count FROM subscriptions`))[0].count as any
+    );
+  }
+});
 
 export const timeOutgoingRequest = new client.Histogram({
   name: 'http_webhook_duration_seconds',
