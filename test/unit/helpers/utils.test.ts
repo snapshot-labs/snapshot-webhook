e354bd8002e231084dbf641d0d958af3610e5696
Wan Qi Chen <495709+wa0x6e@users.noreply.github.com>
2023-11-28 14:52:54 +0400

    chore: setup tests

---
+++ test/unit/helpers/utils.test.ts
@@ -0,0 +1,7 @@
+import { getProposal } from '../../../src/helpers/utils';
+
+describe('getProposal()', () => {
+  it.todo('returns null when the proposal is flagged');
+  it.todo('returns null when the proposal does not exist');
+  it.todo('returns the proposal');
+});
