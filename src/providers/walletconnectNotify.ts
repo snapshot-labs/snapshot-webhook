6b691e856fe3aed394527bfe94d0b80e97f06c7b
Chris Smith <chris@walletconnect.com>
2024-01-23 21:24:15 -0500

    fix: use chain agnostic notify

--- src/providers/walletconnectNotify.ts
+++ src/providers/walletconnectNotify.ts
@@ -23,56 +23,6 @@ const PER_SECOND_RATE_LIMIT = 2;
 const WAIT_ERROR_MARGIN = 0.25;
 const WAIT_TIME = 1 / PER_SECOND_RATE_LIMIT + WAIT_ERROR_MARGIN;
 
-// Fetch subscribers from WalletConnect Notify server
-export async function getSubscribersFromWalletConnect() {
-  const fetchSubscribersUrl = `${WALLETCONNECT_NOTIFY_SERVER_URL}/${WALLETCONNECT_PROJECT_ID}/subscribers`;
-
-  try {
-    const subscribersRs = await fetch(fetchSubscribersUrl, {
-      headers: AUTH_HEADER
-    });
-
-    const subscribers: string[] = await subscribersRs.json();
-
-    return subscribers;
-  } catch (e) {
-    capture(e);
-    console.log('[WalletConnect] failed to fetch subscribers');
-    return [];
-  }
-}
-
-// Find the CAIP10 of subscribers, since the Notify API requires CAIP10.
-async function crossReferenceSubscribers(
-  space: { id: string },
-  spaceSubscribers
-) {
-  const subscribersFromDb = spaceSubscribers;
-  const subscribersFromWalletConnect = await getSubscribersFromWalletConnect();
-
-  // optimistically reserve all subscribers from the db
-  const crossReferencedSubscribers = new Array(subscribersFromDb.length);
-
-  // Create a hashmap for faster lookup
-  const addressPrefixMap = new Map<string, string>();
-  for (const subscriber of subscribersFromWalletConnect) {
-    const unprefixedAddress = subscriber.split(':').pop();
-    if (unprefixedAddress) {
-      addressPrefixMap.set(unprefixedAddress, subscriber);
-    }
-  }
-
-  for (const subscriber of subscribersFromDb) {
-    const crossReferencedAddress = addressPrefixMap.get(subscriber);
-    if (crossReferencedAddress) {
-      crossReferencedSubscribers.push(crossReferencedAddress);
-    }
-  }
-
-  // remove empty elements from the array, since some might not have been found in WalletConnect Notify server
-  return crossReferencedSubscribers.filter(addresses => addresses);
-}
-
 async function queueNotificationsToSend(notification, accounts: string[]) {
   for (let i = 0; i < accounts.length; i += MAX_ACCOUNTS_PER_REQUEST) {
     await sendNotification(
@@ -138,16 +88,14 @@ function formatMessage(event: Event, proposal) {
   };
 }
 
-export async function send(event: Event, proposal, subscribers: string[]) {
+export async function send(event: Event, proposal, addresses: string[]) {
   if (event.event !== 'proposal/start') return;
-  const crossReferencedSubscribers = await crossReferenceSubscribers(
-    proposal.space,
-    subscribers
-  );
   const notificationMessage = formatMessage(event, proposal);
+
+  const accounts = addresses.map(address => `eip155:1:${address}`);
 
   await queueNotificationsToSend(
     notificationMessage,
-    crossReferencedSubscribers
+    accounts,
   );
 }
